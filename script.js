0addd7710338beac796524d95ddda5ab2b2212ee
Shane T Odlum <shane.odlum@consensys.net>
2022-04-06 12:14:41 +0100

    dont wait for dom readY

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const sign = document.getElementById('sign');
-  const address = document.getElementsByName('address')[0];
-  const sign_result = document.getElementById('sign_result');
+const sign = document.getElementById('sign');
+const address = document.getElementsByName('address')[0];
+const sign_result = document.getElementById('sign_result');
 
-  sign.addEventListener('click', async () => {
+sign.addEventListener('click', async () => {
 
-    console.log('DOMContentLoaded');
+  console.log('Clicked sign button')
 
-    await window.ethereum.enable();
-    const payload = {
-      method : 'eth_sign',
-      params : [address.value, '0x' + '0'.repeat(64)]
-    };
+  await window.ethereum.enable();
+  const payload = {
+    method : 'eth_sign',
+    params : [address.value, '0x' + '0'.repeat(64)]
+  };
 
-    try {
-      const result = await window.ethereum.request(payload)
-      console.log(result);
-      sign_result.value = result;
-    } catch (e) {
-      sign_result.value = e.message;
-    }
-  })
+  try {
+    const result = await window.ethereum.request(payload)
+    console.log(result);
+    sign_result.value = result;
+  } catch (e) {
+    sign_result.value = e.message;
+  }
+
 });
